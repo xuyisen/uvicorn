b58c6c51af8a312df59bcc936cd27ee2cc721f90
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-23 15:54:18 -0400

    xuyisen__diff__id_307

--- uvicorn/loops/asyncio.py
+++ uvicorn/loops/asyncio.py
@@ -6,6 +6,6 @@ from collections.abc import Callable
 
 
 def asyncio_loop_factory(use_subprocess: bool = False) -> Callable[[], asyncio.AbstractEventLoop]:
-    if sys.platform == "win32" and not use_subprocess:  # pragma: py-win32
+    if sys.platform == "win32" and not use_subprocess:  # pragma: py-not-win32
         return asyncio.ProactorEventLoop
     return asyncio.SelectorEventLoop  # pragma: py-not-win32
--- uvicorn/loops/auto.py
+++ uvicorn/loops/auto.py
@@ -7,7 +7,7 @@ from collections.abc import Callable
 
 def auto_loop_factory(use_subprocess: bool = False) -> Callable[[], asyncio.AbstractEventLoop]:
     try:
-        if sys.platform == "win32":  # pragma: py-win32
+        if sys.platform == "win32":  # pragma: py-not-win32
             from uvicorn.loops.winloop import winloop_loop_factory as loop_factory
         else:  # pragma: py-not-win32
             from uvicorn.loops.uvloop import uvloop_loop_factory as loop_factory
